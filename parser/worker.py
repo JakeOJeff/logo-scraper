95d48023c5aab9f53c811e70cb902e0f61218e97
Jake <realjakeojeff@gmail.com>
2026-05-14 17:15:10 +0530

    temporary stashing of worker

--- parser/worker.py
+++
@@ -1,7 +0,0 @@
-
-
-async def scrape_urL(page, url)
-    try:
-        response = await page.goto(url, timeout=30000, wait_until='domcontentloaded')
-
-        html = await()
--- scraper.py
+++ scraper.py
@@ -1,22 +1,21 @@
-import asyncio
-from playwright.async_api import async_playwright, Playwright
+from playwright.sync_api import sync_playwright, Playwright
 from parser import psg, parse
 import csv
 
 def run(playwright: Playwright):
     chromium = playwright.chromium
-    browser = await chromium.launch(headless=True)
+    browser = chromium.launch()
 
     psg.clearAll() # i added db clear to test
     psg.create()
 
-    page = await browser.new_page()
+    page = browser.new_page()
 
     with open("websites.csv") as f:
         for line in f:
             url = "https://" + line.strip()
             try:
-                page.goto(url, timeout=30000)
+                page.goto(url, timeout=10000)
                 page.wait_for_load_state('domcontentloaded', timeout=10000)
                 html = page.content()
                 psg.insertHtml(url, html)
@@ -24,12 +23,7 @@ def run(playwright: Playwright):
                 print(f"failed {url} - {e}")
                 with open('output/failed.csv', 'a', newline='') as f:
                     writer = csv.writer(f)
-                    writer.writerow([url, str(e).split('\n')[0]])
-                
-                try:
-                    page.goto('about:blank', timeout=50000)
-                except:
-                    pass
+                    writer.writerow([url, e])
                 continue
 
     psg.parseDBHtml()
