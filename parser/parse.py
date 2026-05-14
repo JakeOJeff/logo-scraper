8118f5be772d4b71388c8a03e12849614e4a5edf
Jake <realjakeojeff@gmail.com>
2026-05-14 09:12:24 +0530

    Fixed svglogo finder, this is used as last resort

--- parser/parse.py
+++ parser/parse.py
@@ -25,6 +25,16 @@ def parseHtml(url,html):
     link = soup.find('link', attrs={'rel': 'apple-touch-icon'})
     linkTagAppleIcon = link.get('href') if link else None
     
+    svgLogo = None
+    for svg in soup.find_all('svg'):
+        classes = ' '.join(svg.get('class', []))
+        identity = svg.get('id', '')
+        aria = svg.get('aria-label', '')
+
+        if any('logo' in x.lower() for x in [classes, identity, aria]):
+            svgLogo = f"{aria or classes or identity}"
+            break
+
     imgAlt = None
     for img in soup.find_all('img'):
         alt = img.get('alt', '')
@@ -32,7 +42,7 @@ def parseHtml(url,html):
             imgAlt = img.get('src')
             break
 
-    logo = ogImage or schemaJson or metaTagContent or linkTagAppleIcon or imgAlt
+    logo =  ogImage or schemaJson or metaTagContent or linkTagAppleIcon or imgAlt or svgLogo
 
     if logo:
         print(f"{url} | {logo}")
